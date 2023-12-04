e1cc027c01ec0a2a8eb59fa4594a92bcea5b69a3
miladhzz <miladhzz@gmail.com>
2023-12-04 17:37:40 +0330

    init soft delete

--- shop/models.py
+++ shop/models.py
@@ -2,46 +2,52 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class Category(models.Model):
-    title = models.CharField(max_length=100)
+class BaseModel(models.Model):
+    deleted = models.BooleanField(null=True, blank=True, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(BaseModel):
+    title = models.CharField(max_length=100)    
 
     def __str__(self):
         return self.title
 
 
-class Product(models.Model):
+class Product(BaseModel):
     title = models.CharField(max_length=100)
     content = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=10)
     image = models.ImageField()
     quantity = models.PositiveIntegerField()
     status = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     
     def __str__(self):
         return self.title
 
 
-class Cart(models.Model):
+class Cart(BaseModel):
     quantity = models.PositiveIntegerField()
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
-class Order(models.Model):
+class Order(BaseModel):
     total_price = models.DecimalField(decimal_places=2, max_digits=10)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.BooleanField(null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
 
 
-class OrderProduct(models.Model):
+class OrderProduct(BaseModel):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
     price = models.DecimalField(decimal_places=2, max_digits=10)
-    created_at = models.DateTimeField(auto_now_add=True)
 
 
 class PaymentLog(models.Model):
