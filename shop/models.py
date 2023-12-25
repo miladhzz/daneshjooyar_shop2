6f2dd725f8e7a563fbe0f58879a940a691fa22b0
miladhzz <miladhzz@gmail.com>
2023-12-25 14:00:56 +0330

    implement get deleted reocrds

--- shop/models.py
+++ shop/models.py
@@ -2,10 +2,17 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+class BaseModelManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(deleted=False)
+
+
 class BaseModel(models.Model):
-    deleted = models.BooleanField(null=True, blank=True, editable=False)
+    deleted = models.BooleanField(default=False, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    objects = BaseModelManager()
 
     class Meta:
         abstract = True
