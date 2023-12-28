d19d39154e032fb6c58b187af65bda5955160b14
miladhzz <miladhzz@gmail.com>
2023-12-29 01:14:23 +0330

    complete ModelAdmin for soft delete

--- shop/admin.py
+++ shop/admin.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.Cart)
-admin.site.register(models.Order)
-admin.site.register(models.OrderProduct)
-admin.site.register(models.Product)
-
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['id', 'title']
@@ -16,4 +11,43 @@ class CategoryAdmin(admin.ModelAdmin):
             category.delete()
 
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'price', 'quantity', 'status']
+    search_fields = ['title']
+    list_filter = ['status']
+
+    def delete_queryset(self, request, queryset):
+        for product in queryset:
+            product.delete()
+
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['id', 'product', 'user', 'quantity']
+
+    def delete_queryset(self, request, queryset):
+        for cart in queryset:
+            cart.delete()
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'total_price', 'status']
+    list_filter = ['status']
+
+    def delete_queryset(self, request, queryset):
+        for order in queryset:
+            order.delete()
+
+
+class OrderProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'order_id', 'product', 'quantity', 'price']
+
+    def delete_queryset(self, request, queryset):
+        for order_product in queryset:
+            order_product.delete()
+
+
 admin.site.register(models.Category, CategoryAdmin)
+admin.site.register(models.Product, ProductAdmin)
+admin.site.register(models.Cart, CartAdmin)
+admin.site.register(models.Order, OrderAdmin)
+admin.site.register(models.OrderProduct, OrderProductAdmin)
