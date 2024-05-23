e87cbaac968b9d0cd2d20a2aeaadc86cc6bb1e6b
miladhzz <miladhzz@gmail.com>
2024-05-23 17:18:46 +0330

    implement login form

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "accounts"
 
 urlpatterns = [
-    path('edit-profile/', views.edit_profile, name="edit_profile"),
-    path('get-cities/', views.get_cities, name="get_cities"),
+    path('edit-profile/', views.edit_profile, name='edit_profile'),
+    path('get-cities/', views.get_cities, name='get_cities'),
+    path('login/', views.login, name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import LoginForm
 from .models import City
+from django.contrib.auth import authenticate, login as django_login
+
 
 def edit_profile(request):
     return HttpResponse('<h1>Edit Profile</h1>')
@@ -13,3 +17,23 @@ def get_cities(request):
 
     cities = City.objects.filter(province_id=province_id).values('id', 'title')
     return JsonResponse(list(cities), safe=False)
+
+
+def login(request):
+    next_page = request.GET.get('next')
+    if request.method == 'GET':
+        form = LoginForm()
+        return render(request, 'login.html', {'form': form})
+
+    form = LoginForm(request.POST)
+    if form.is_valid():
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            django_login(request, user)
+            return redirect(next_page)
+        messages.error(request, 'Invalid username or password')
+        return render(request, 'login.html', {'form': form})
+
+    return render(request, 'login.html', {'form': form})
