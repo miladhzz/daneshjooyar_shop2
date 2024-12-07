8338e54b8f358c53ee2c6cb47655b67b00568163
Milad Hatami <miladhzz@gmail.com>
2024-12-07 13:47:40 +0330

    fix models

--- discount/models.py
+++ discount/models.py
@@ -7,14 +7,14 @@ from core import DiscountType
 
 class DiscountPrice(BaseModel):
     name = models.CharField(max_length=255)
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    product = models.ManyToManyField(Product, null=True, blank=True)
+    category = models.ManyToManyField(Category, null=True, blank=True)
     start_time = models.DateTimeField(default=timezone.now)
     end_time = models.DateTimeField(null=True, blank=True)
     status = models.BooleanField(default=False)
     type = models.CharField(max_length=10, choices=DiscountType.CHOICES, default=DiscountType.FIXED)
     percent = models.PositiveIntegerField(null=True, blank=True)
-    fixed = models.PositiveIntegerField(null=True, blank=True)
+    fixed = models.PositiveIntegerField(default=0, null=True, blank=True)
 
     def __str__(self):
         return self.name
