d938c148d9807beb3212c33a9b5e34bb4529b17b
miladhzz <miladhzz@gmail.com>
2024-12-16 01:58:04 +0330

    fix bug show cart detail

--- checkout/urls.py
+++ checkout/urls.py
@@ -6,6 +6,6 @@ app_name = "checkout"
 urlpatterns = [
     path('', views.Checkout.as_view(), name="checkout"),
     path('add-to-cart/', views.AddToCart.as_view(), name="add_to_cart"),
-    path('', views.CartDetail.as_view(), name="cart_detail"),
+    path('cart/', views.CartDetail.as_view(), name="cart_detail"),
     path('remove/<int:product_id>/', views.RemoveFromCart.as_view(), name="remove_from_cart"),
 ]
