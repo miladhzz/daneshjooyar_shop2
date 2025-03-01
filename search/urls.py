8204c79172a45281229ea17901f04df85adee85f
miladhzz <miladhzz@gmail.com>
2025-03-01 20:09:14 +0330

    init search view without front paginate

--- search/urls.py
+++ search/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = "search"
 
 urlpatterns = [
-    path('search/', views.search, name="search"),
+    path('', views.SearchView.as_view(), name="search"),
 ]
--- search/views.py
+++ search/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from shop.models import Product, Category
+# این اضافه شد
+from django.db.models import Count
 
 
-def search(request):
-    return None
+
+class SearchView(ListView):
+    model = Product
+    template_name = 'search.html'
+    context_object_name = 'products'
+    paginate_by = 3
+
+    def get_queryset(self):
+        query = self.request.GET.get('q', '')
+        return Product.objects.filter(title__icontains=query).order_by('title')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context['categories'] = Category.objects.all()  اول این
+        # بعد این اضافه میشه
+        context['categories'] = Category.objects.annotate(product_count=Count('product')).filter(product_count__gt=0)
+        # که حذف شده های منطقی رو هندل نکرده
+        return context
