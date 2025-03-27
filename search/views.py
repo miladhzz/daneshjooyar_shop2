f240034628de180a731da411691b8552220e63e4
miladhzz <miladhzz@gmail.com>
2025-03-27 23:06:06 +0330

    show categories count

--- search/views.py
+++ search/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView
-from shop.models import Product
+from shop.models import Product, Category
+from django.db.models import Count
 
 
 class SearchView(ListView):
@@ -11,3 +12,8 @@ class SearchView(ListView):
     def get_queryset(self):
         query = self.request.GET.get('q', '')
         return Product.objects.filter(title__icontains=query).order_by('title')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.annotate(product_count=Count('product')).filter(product_count__gt=0)
+        return context
